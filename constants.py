82e786c99a2872ca913e637fe11c8797b58a8d1a
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-05-21 08:32:58 -0300

    Error added: id not found

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 ERRORS = [
     {"no_students_added" : "Você não adicionou nenhum aluno."},
+    {"id_not_found": "Id não encontrado."}
 ]
 
 SUCCESS = [
     {"student_added": "Aluno cadastrado!"},
 ]
-
