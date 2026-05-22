9e030fd5d705bcf4c72a07d41725f5171ae636c5
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-05-22 10:37:03 -0300

    adding rule bussines

--- core.py
+++ core.py
@@ -1,40 +1,36 @@
-from rich import print
 from time import sleep
 import service, msg, constants as const
 
 def sending_new_student(local_file:str) -> bool:
     new_student = register_student()
-    if not new_student:
-        error = msg.msg_erro('ERRO! Usuário digitou um nome inválido.')
-        msg.print_formatted(error)
+    if new_student == "Usuário digitou um nome inválido.":
+        msg.feedback_for_user(new_student, "error")
         return False
     
     student_json = service.adding_student_to_json(local_file, new_student)
     if student_json == "Aluno cadastrado!":
-        ok = msg.msg_success(student_json)
-        msg.print_formatted(ok)
+        msg.feedback_for_user(student_json, "success")
         return True
     
-    error = msg.msg_erro(student_json)
-    msg.print_formatted(error)
+    msg.feedback_for_user(student_json, "error")
     return False
 
 
 def list_student(local_file: str) -> bool:
     data = service.reading_file(local_file)
-    if not data == "Você não adicionou nenhum aluno.":
-        cont = 0
-        print('ID - NOME - IDADE')
-        for item in data:
-            cont +=1
-            print(f'{cont} - {item['name']} - {item['age']}')
-        print()
-        sleep(1)
-        return True
+    if data == "Você não adicionou nenhum aluno.":
+        msg.feedback_for_user(data, "error")
+        return False
     
-    erro = msg.msg_erro(data)
-    msg.print_formatted(erro)
-    return False
+    cont = 0
+    print('ID - NOME - IDADE')
+    for item in data:
+        cont +=1
+        print(f'{cont} - {item['name']} - {item['age']}')
+    print()
+    sleep(1)
+    return True
+
 
 def update_student(local_file: str) -> bool:
     list_student(local_file)
@@ -43,26 +39,47 @@ def update_student(local_file: str) -> bool:
     new_student = register_student()
     
     if not new_student:
-        erro = msg.msg_erro('ERRO! Usuário digitou um nome inválido.')
-        msg.print_formatted(erro)
+        msg.feedback_for_user("Usuário digitou um nome inválido.", "error")
         return False
     
     content = service.updating_student_in_json(local_file, new_student, opc_id)
+    if content == "Você não adicionou nenhum aluno.":
+        msg.feedback_for_user(content, "error")
+        return False
+    
+    elif content == "Id não encontrado.":
+        msg.feedback_for_user(content, "error")
+        return False
 
-    if content == "Aluno atualizado!":
-        success = msg.msg_success(content)
-        msg.print_formatted(success)
-
+    msg.feedback_for_user(content, "success")
     return True
 
 
 def delete_student(local_file: str) -> bool:
     list_student(local_file)
     opc_id = register_id_student()
-    service.deleting_student(local_file, opc_id)
+    
+    if opc_id == "Usuário digitou um valor inválido.":
+        msg.feedback_for_user(opc_id, "error")
+        return False
+    
+    content = service.deleting_student(local_file, opc_id)
+    if content == "Você não adicionou nenhum aluno.":
+        msg.feedback_for_user(content, "error")
+        return False
+    
+    elif content == "Id não encontrado.":
+        msg.feedback_for_user(content, "error")
+        return False
+    
+    msg.feedback_for_user(content, "success")
+    return True
+    
 
 
-def register_student() -> dict:
+
+
+def register_student() -> dict | str:
     name = input('Nome aluno: ').strip().capitalize()
     if len(name) < 2:
         return const.ERRORS[3]["invalid_name"]
@@ -75,6 +92,7 @@ def register_student() -> dict:
         return new_student
     except ValueError:
         return const.ERRORS[2]["value_error"]
+
 
 def register_id_student() -> int:
     try:
